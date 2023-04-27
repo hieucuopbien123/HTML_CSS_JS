3fc800761771ab14619c19eaf5ef34e003f7f9be
Ryan.Nguyen <Ryan.Nguyen@avepoint.com>
2023-04-27 14:16:11 +0700

    x

---
+++ Code/HTML_CSS_JS/Advance/components/searchbaranimation/index.js
@@ -0,0 +1,16 @@
+// updated 2019
+
+
+//  old version / jquery
+//
+// function expand() {
+//   $(".search").toggleClass("close");
+//   $(".input").toggleClass("square");
+//   if ($('.search').hasClass('close')) {
+//     $('input').focus();
+//   } else {
+//     $('input').blur();
+//   }
+// }
+// $('button').on('click', expand);
+//
