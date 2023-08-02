c7c55d246415e9a3e4c5b0af0da328a764fb4ae8
Ryan.Nguyen <Ryan.Nguyen@avepoint.com>
2023-08-02 15:39:31 +0700

    a

---
+++ Code/HTML_CSS_JS/Advance/MusicApp/js/constants.js
@@ -0,0 +1,8 @@
+const API_KEY = "AIzaSyCzgPDerIGI_Ui-DhCzUHiji1HsiE9C2WY";
+
+const NOT_FOUND_SONG = "The song is not found";
+
+const VIDEO_YOUTUBE_ID = "video_youtube_id";
+const SELECTED_VIDEO_ID = "id-selected";
+
+const NOT_FOUND = "not found";
